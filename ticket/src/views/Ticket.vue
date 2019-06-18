bb4ee28ec82be5f1967f849264d0becc4d048563
Josh Wasserman <joshbwasserman@gmail.com>
2019-06-18 15:09:14 -0400

    began scaffold on Ticket.vue

--- ticket/src/views/Ticket.vue
+++ ticket/src/views/Ticket.vue
@@ -2,8 +2,12 @@
   <h1>Ticket</h1>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+import TicketForm from "../components/ticketForm.vue";
+export default class Ticket extends Vue {}
 </script>
 
 <style>
