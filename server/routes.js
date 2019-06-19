a70aaf003ae18602d5aaf1d439c694fc794bf3b0
Josh Wasserman <github email address>
2019-06-19 12:35:40 -0400

    Started json schema dev

--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,13 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+let Ajv = require('ajv');
 let router = express.Router();
+
+let formSchema = require('./FormSchema.json');
 
 router.use(bodyParser.json());
 
-let data = {};
+let data = formSchema;
 
 router.get('/', (req, res) => {
   res.send(data);
