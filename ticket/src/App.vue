fbc4c674f05758e0663ec5158266b4b5f96e2d59
Josh Wasserman <joshbwasserman@gmail.com>
2019-06-19 15:46:18 -0400

    Added vuetify to run sample ticket. Continued building the schema.

--- ticket/src/App.vue
+++ ticket/src/App.vue
@@ -3,7 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/Ticket">Ticket</router-link> |
-      <router-link to="/about">About</router-link> 
+      <router-link to="/about">About</router-link> |
+      <router-link to="/sampleTicket">sampleTicket</router-link> 
     </div>
     <router-view/>
   </div>
