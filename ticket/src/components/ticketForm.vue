591b6d650308a41608c86913355e7d80cc4c8a6b
Josh Wasserman <joshbwasserman@gmail.com>
2019-06-20 12:27:02 -0400

    added form criteria

---
+++ ticket/src/components/ticketForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <form action @submit="handleSubmit($event)">
+      <div class="row">
+        <div class="form-group col">
+          <label for="Symbol">Symbol</label>
+          <input type="text">
+        </div>
+      </div>
+
+      
+    </form>
+  </div>
+  
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { State, Getter, Action, Mutation } from "vuex-class";
+import { Watch } from "vue-property-decorator";
+
+@Component({})
+export default class TicketForm extends Vue {
+  private symbol: any = null;
+  private bps: any = null;
+  private tgtBps: any = null;
+  private quantity: any = null;
+  private notional: any = null;
+  private orderType: any = null;
+  private price: any = null;
+  private strat1: any = null;
+  private strat2: any = null;
+  private type: any = null;
+  private portfolio: any = null;
+  private isSwap: any = null;
+
+  handleSubmit = (e: any) => {
+    console.log(e);
+  };
+}
+</script>
+
+<style>
+</style>
