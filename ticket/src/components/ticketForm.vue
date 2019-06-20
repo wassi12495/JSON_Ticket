c76144cc86b11e5cf000bcb6d78c191be1457df6
Josh Wasserman <joshbwasserman@gmail.com>
2019-06-20 14:18:51 -0400

    schema accesible in ticket form.

--- ticket/src/components/ticketForm.vue
+++ ticket/src/components/ticketForm.vue
@@ -4,23 +4,36 @@
       <div class="row">
         <div class="form-group col">
           <label for="Symbol">Symbol</label>
-          <input type="text" v-model="symbol">
+          <v-text-field type="text" v-model="symbol"/>
           <label for="BPS">BPS</label>
-          <input type="text" v-model="bps">
+          <v-text-field type="text" v-model="bps"/>
           <label for="tgtBps">tgtBps</label>
-          <input type="text" v-model="tgtBps">
-          <label for="Symbol">Symbol</label>
-          <input type="text">
-          <label for="Symbol">Symbol</label>
-          <input type="text">
-          <label for="Symbol">Symbol</label>
-          <input type="text">
-          <label for="Symbol">Symbol</label>
-          <input type="text">
+          <v-text-field type="text" v-model="tgtBps"/>
+          <label for="quantity">quantity</label>
+          <v-text-field type="text" v-model="quantity"/>
+          <label for="notional">notional</label>
+          <v-text-field type="text" v-model="notional"/>
+          <label for="orderType">orderType</label>
+          <v-text-field type="text" v-model="orderType"/>
         </div>
-      </div>
+        <div class="row">
+          <label for="price">price</label>
+          <input type="text" v-model="price">
+          <label for="strat1">strat1</label>
+          <input type="text" v-model="strat1">
+          <label for="strat2">strat2</label>
+          <input type="text" v-model="strat2">
+          <label for="type">type</label>
+          <input type="text" v-model="type">
+          <label for="portfolio">portfolio</label>
+          <input type="text" v-model="portfolio">
+          <label for="isSwap">isSwap</label>
+          <input type="text" v-model="isSwap">
+          
+        </div>
+       <input class="btn btn-primary" type="submit">
 
-      
+      </div>
     </form>
   </div>
   
@@ -34,6 +47,8 @@ import { Watch } from "vue-property-decorator";
 
 @Component({})
 export default class TicketForm extends Vue {
+  @State("formSchema") formSchema: any;
+
   private symbol: any = null;
   private bps: any = null;
   private tgtBps: any = null;
@@ -50,6 +65,10 @@ export default class TicketForm extends Vue {
   handleSubmit = (e: any) => {
     console.log(e);
   };
+  @Watch("symbol")
+  onSymbolChange() {
+    console.log(this.symbol);
+  }
 }
 </script>
 
